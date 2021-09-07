eb1668f31a82d49e21e8192680619a4b74288c36
Corty-Plato <corentin.chanet@gmail.com>
2021-09-07 16:25:39 +0200

    Improving regex and tf_idf

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ import time
 
 start = time.time()
 
-from parsing.extraction import parsing_documents, RegxSections, RegxInfos, create_corpus
+from parsing.extraction import parsing_documents, RegxSections, RegxInfos, create_corpus, create_corpus_combinations
 from matching.tf_idf_embeddings import tf_idf_func
 from sklearn.metrics.pairwise import linear_kernel
 import re
 
-documents = parsing_documents(3266, RegxSections, RegxInfos)
+documents = parsing_documents(3000, RegxSections, RegxInfos)
 
-corpus = create_corpus(documents)
+corpus = create_corpus_combinations(documents)
 
 tf_idf_corpus = tf_idf_func(corpus)
 
-cosine_similarities = linear_kernel(tf_idf_corpus['work_exp_matrix'][24:25], tf_idf_corpus['work_exp_matrix']).flatten()
-indices = cosine_similarities.argsort()[:-5:-1]
+cosine_similarities = linear_kernel(tf_idf_corpus['work_exp_matrix'][778], tf_idf_corpus['work_exp_matrix']).flatten()
+indices = cosine_similarities.argsort()[:-21:-1]
+scores = sorted(cosine_similarities)[:-21:-1]
 
 print(time.time()-start)
 #print(sorted(list(results[24]['education'].items()), key=lambda x: x[1]))
--- parsing/extraction.py
+++ parsing/extraction.py
@@ -4,16 +4,17 @@ from enum import Enum
 import joblib
 from parsing.flair_ner import _extract_persons, _extract_work
 import pandas as pd
+from itertools import combinations
 
 #tagger = joblib.load("../../NLP-resume/processing/ner_onto_large.pkl")
 
 class RegxSections(Enum):
     work_exp = r'((teaching|exp.{3,4}nce|employment|work|working|career|job.?|professional|organizational|industr.{1,3}|previous.{0,2}|present.{0,2}|current.{0,2}|detail.{0,2})' \
            r'.{0,4}' \
-           r'(record.?|role.?|responsibilit.{1,4}|exposure|synopsis|detail.?|exp.{3,4}nce|employ.{2,4}|overview|background|highlight|summar.{1,3}|histor.{0,4}|position.?|ap.?oint.?ment.?)' \
-           r'|^.{0,6}internship|^.{0,6}experience|^.{0,6}career.{0,4}$|^.{0,6}job.{0,5}$|^.?personal experience$|^.{0,6}responsibilit.{1,6}$|^.{0,6}dut.{1,3}|academic.?.{1,4}achievement.?)'
+           r'(record.?|role.?|responsibilit.{1,4}|exposure|synopsis|detail.?|profile.?|exp.{3,4}nce|employ.{2,4}|overview|background|highlight|summar.{1,3}|histor.{0,4}|position.?|ap.?oint.?ment.?)' \
+           r'|^.{0,6}internship|^.{0,6}experience|^.{0,6}career.{0,4}$|^.{0,6}job.{0,5}$|^.?personal experience$|^.{0,6}responsibilit.{1,6}$|^.{0,6}dut.{1,3}|^.?assig.{0,2}ment.?)'
     education = r'(school.{0,5}|schola.{1,5}|project.?|vocation.{1,4}|academi.{1,4}|education.{0,4}|university|college|high school|graduat.{1,4}|degree.?).{0,4}(training.?|qualifi.{2,9}|detail.?|purview|project.?|record.?|overview|training|background|highlight|summar.{1,3}|histor.{0,4}|course.?|credential.?|profile)|^.?education.{0,6}$|^.?certificat.{1,4}|^.?qualifi.{1,6}$|professional qualifi.{2,9}|professional training|^.?schola.{1,5}$|^.{0,6}achievement.{0,5}$|additional qualific|academi.{1,4}'
-    skills = r'(techn.{2,8}|core|key|area.?|professional|additional|organization.{0,2}|software|it|industry|personal.{0,3}|computer|relevant|hard.{0,5}|soft|practical).{0,4}(skil.{1,2}|trait.?|knowledge|strength.?|proficiency|competenc.{1,4}|capabilit.{1,4}|capacit.{1,4}|abilit.{1,4}|experti.e|literacy)|^.?skill.{0,4}|^.?strength.{0,4}|^.?proficienc.{1,3}'
+    skills = r'(techn.{2,8}|core|key|area.?|professional|additional|organization.{0,2}|software|it|industry|personal.{0,3}|computer|relevant|hard.{0,5}|soft|practical).{0,4}(skil.{1,10}$|trait.?|knowledge|strength.?|proficiency|competenc.{1,4}|capabilit.{1,4}|capacit.{1,4}|abilit.{1,4}|experti.e|literacy)|^.?skill.{0,4}|^.?strength.{0,4}|^.?proficienc.{1,3}'
     hobbies = r'(extra|activit.{1,4}).{0,12}(activit.{1,3})|^.?hobb.{1,3}|^.?activit.{1,4}'
     interests = r'(area.{0,1}|field.?).{0,4}(interest.{0,1})'
     personal_info = r'(person.{0,2}|contact|about|identity|passport).{0,4}(detail.?|info.{0,9}|profile|data)|^.?information$|address'
@@ -84,6 +85,35 @@ def create_corpus(documents:dict):
         else:
             for pattern in RegxSections:
                 corpus[pattern.name].append("")
+
+    return corpus
+
+def create_corpus_combinations(documents:dict):
+
+    combinations_list = []
+    sections_names = [pattern.name for pattern in RegxSections]
+
+    for i in range(1,5,1):
+        combinations_list.extend(list(combinations([pattern for pattern in sections_names[:4]], i)))
+
+    corpus = {"+".join(combination):[] for combination in combinations_list}
+    print(combinations_list)
+    for combination in combinations_list:
+        print(f"Combination {combination} is being processed")
+        for i in range(1,len(documents)+1,1):
+            if documents[i]:
+                content = []
+                for section in combination:
+                    content.extend(documents[i]['sections'][section])
+                if content:
+                    text = " ".join(content)
+                    corpus["+".join(combination)].append(text)
+                else:
+                    corpus["+".join(combination)].append("")
+            else:
+                for combination_name in corpus.keys():
+                    corpus[combination_name].append("")
+
     return corpus
 
 #joblib.dump(documents, "documents.pkl")
