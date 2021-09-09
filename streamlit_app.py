38d8895827cfcab43473a1ec6213e03f760d0fbd
Misterkadrix <kad-sa@outlook.fr>
2021-09-09 14:22:10 +0200

    Some changes on Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,71 +9,16 @@ from tkinter import filedialog
 from io import StringIO
 
 
-st.title('My first app')
-st.write('Hello World')
-st.write("Here's our first attempt at using data to create a table:")
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+st.title('Radix Matching')
 
-df
+#Here we will upload a file
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
-option = st.sidebar.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected:', option
-
-left_column, right_column = st.columns(2)
-pressed = left_column.button('Press me?')
-if pressed:
-  right_column.write("Is this real life! It's just fantasy  ")
-
-expander = st.expander("FAQ")
-expander.write("Here you could put in some really, really long explanations...")
-
-'Starting a long computation...'
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-"""for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
-"""
-if st.button('Upload file'):
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename()
-    st.image(file_path)
-
-"""uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
-if uploaded_file is not None:
-    df = extract_data(uploaded_file)"""
-
-uploaded_file = st.file_uploader("Choose a file")
+st.write('Upload the resume you want to match')
+uploaded_file = st.file_uploader("")
 if uploaded_file is not None:
      # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+    #st.write(bytes_data)
 
      # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
@@ -86,3 +31,5 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     #dataframe = pd.read_csv(uploaded_file)
     #st.write(dataframe)
+
+#Here i ll try to mark
